b7f4612668741806d7f9581f2f9cc23a7f32de40
camireinag <79527555+camireinag@users.noreply.github.com>
2021-03-04 09:24:17 -0500

    Revert "Merge branch 'main' of https://github.com/CjayZambrano/Taller-Dados into main"
    This reverts commit faf2da21e1fca0a00fa639faeddf38c18511ceae, reversing
    changes made to 0c6c009fdaa229250a2c549f44e44b788e4dcde1.

---
+++ classes/pila/src/pila/Base.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pila;
+
+/**
+ *
+ * @author david_orlando.mena@uao.edu.co David Orlando Mena 2180448 
+ * @author frank_dan.castillo@uao.edu.co Frank Daniel Castillo 2171721
+ * @author cjay.zambrano@uao.edu.co Cjay Zambrano Liñan 2151536
+ * @author maria_camila.reina@uao.edu.co Maria Camila Reina 2190037
+ * @date Feb 2021 
+ */ 
+public abstract class Base {
+    
+    public abstract Base copy();
+    
+}
