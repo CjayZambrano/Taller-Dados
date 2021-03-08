691d7f7990046bc5364ed99f59f8fe54e2e5e243
camireinag <79527555+camireinag@users.noreply.github.com>
2021-03-08 15:49:34 -0500

    z

--- src/control/FXMLDocumentController.java
+++ src/control/FXMLDocumentController.java
@@ -28,6 +28,7 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
+import modelo.Modelo;
 import pila.OperacionesPila;
 import pila.Tools;
 
@@ -89,7 +90,7 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void btnMostrarUltimoLanzamiento(ActionEvent event) {
-        Pila<Lanzamiento> ultimo = obtenerUltimo(pilaLanzamientos);
+        Pila<Lanzamiento> ultimo = Modelo.obtenerUltimo(pilaLanzamientos);
         JOptionPane.showMessageDialog(null, ultimo.toString());
     }
 
@@ -102,7 +103,7 @@ public class FXMLDocumentController implements Initializable {
     private void btnNumRepetidos(ActionEvent event) {
         int valor;
         valor = Integer.parseInt(txtvalorRecibido.getText());
-        numRepetidos = obtenerNumRepetido(pilaLanzamientos, valor);
+        numRepetidos = Modelo.obtenerNumRepetido(pilaLanzamientos, valor);
 
         JOptionPane.showMessageDialog(null, "Las veces que ha salido el valor introducido son:" + numRepetidos.toString());
     }
@@ -115,7 +116,7 @@ public class FXMLDocumentController implements Initializable {
         Lanzamiento par = new Lanzamiento();
         valor1 = Integer.parseInt(txtnum1.getText());
         valor2 = Integer.parseInt(txtnum2.getText());
-        Pila<Lanzamiento> parVeces = obtenerPar(pilaLanzamientos, valor1, valor2);
+        Pila<Lanzamiento> parVeces = Modelo.obtenerPar(pilaLanzamientos, valor1, valor2);
         par = parVeces.desapilar();
         vecesPar = par.getNumDado1();
         JOptionPane.showMessageDialog(null, "El número de veces que ha salido este par es de: " + vecesPar + " veces.");
@@ -132,55 +133,6 @@ public class FXMLDocumentController implements Initializable {
 
     }
 
-    @SuppressWarnings(value = "unchecked")
-    public static Pila<Lanzamiento> obtenerPar(Pila<Lanzamiento> pilaOriginal, int valorp1, int valorp2) {
-        Pila<Lanzamiento> pilaPar = new Pila<>();
-        Pila<Lanzamiento> pilaDuplicada = OperacionesPila.duplicar(pilaOriginal);
-        int cont = 0;
-        int valorppp = 0;
-        while (!pilaDuplicada.estaVacia()) {
-            Lanzamiento elemento1 = pilaDuplicada.desapilar();
-            Lanzamiento elemento = elemento1;
-            int valor1 = elemento.getNumDado1();
-            int valor2 = elemento.getNumDado2();
-            if (valor1 == valorp1 && valor2 == valorp2) {
-                cont++;
-            }
-            Lanzamiento nPar = new Lanzamiento(cont, 0);
-            pilaPar.apilar(nPar);
-        }
-        return pilaPar;
-    }
-
-    @SuppressWarnings(value = "unchecked")
-    public static Pila<Lanzamiento> obtenerNumRepetido(Pila<Lanzamiento> pilaOriginal, int valorRepetido) {
-        Pila<Lanzamiento> pilaNumRepetido = new Pila<>();
-        Pila<Lanzamiento> pilaDuplicada = OperacionesPila.duplicar(pilaOriginal);
-        int repetidoDado1 = 0;
-        int repetidoDado2 = 0;
-        while (!pilaDuplicada.estaVacia()) {
-            Lanzamiento elemento1 = pilaDuplicada.desapilar();
-            Lanzamiento elemento = elemento1;
-            int valor1 = elemento.getNumDado1();
-            int valor2 = elemento.getNumDado2();
-            if (valor1 == valorRepetido) {
-                repetidoDado1++;
-            }
-            if (valor2 == valorRepetido) {
-                repetidoDado2++;
-            }
-        }
-        Lanzamiento elemento3 = new Lanzamiento(repetidoDado1, repetidoDado2);
-        pilaNumRepetido.apilar(elemento3);
-        return pilaNumRepetido;
-    }
-
-    public static Pila<Lanzamiento> obtenerUltimo(Pila<Lanzamiento> pilaOriginal) {
-        Pila<Lanzamiento> pilaUltimo = new Pila<>();
-        Pila<Lanzamiento> pilaDuplicada = OperacionesPila.duplicar(pilaOriginal);
-        pilaUltimo.apilar(pilaDuplicada.desapilar());
-        return pilaUltimo;
-    }
     
     
 }
--- src/modelo/Modelo.java
+++ src/modelo/Modelo.java
@@ -5,10 +5,68 @@
  */
 package modelo;
 
+import pila.Lanzamiento;
+import pila.OperacionesPila;
+import pila.Pila;
+
 /**
  *
- * @author usuario
+ * @author david_orlando.mena@uao.edu.co David Orlando Mena 2180448
+ * @author frank_dan.castillo@uao.edu.co Frank Daniel Castillo 2171721
+ * @author cjay.zambrano@uao.edu.co Cjay Zambrano Liñan 2151536
+ * @author maria_camila.reina@uao.edu.co Maria Camila Reina 2190037
+ * @date Feb 2021
  */
 public class Modelo {
+
+    public static Pila<Lanzamiento> obtenerUltimo(Pila<Lanzamiento> pilaOriginal) {
+        Pila<Lanzamiento> pilaUltimo = new Pila<>();
+        Pila<Lanzamiento> pilaDuplicada = OperacionesPila.duplicar(pilaOriginal);
+        pilaUltimo.apilar(pilaDuplicada.desapilar());
+        return pilaUltimo;
+    }
+
+    @SuppressWarnings(value = "unchecked")
+    public static Pila<Lanzamiento> obtenerPar(Pila<Lanzamiento> pilaOriginal, int valorp1, int valorp2) {
+        Pila<Lanzamiento> pilaPar = new Pila<>();
+        Pila<Lanzamiento> pilaDuplicada = OperacionesPila.duplicar(pilaOriginal);
+        int cont = 0;
+        int valorppp = 0;
+        while (!pilaDuplicada.estaVacia()) {
+            Lanzamiento elemento1 = pilaDuplicada.desapilar();
+            Lanzamiento elemento = elemento1;
+            int valor1 = elemento.getNumDado1();
+            int valor2 = elemento.getNumDado2();
+            if (valor1 == valorp1 && valor2 == valorp2) {
+                cont++;
+            }
+            Lanzamiento nPar = new Lanzamiento(cont, 0);
+            pilaPar.apilar(nPar);
+        }
+        return pilaPar;
+    }
+
+    @SuppressWarnings(value = "unchecked")
+    public static Pila<Lanzamiento> obtenerNumRepetido(Pila<Lanzamiento> pilaOriginal, int valorRepetido) {
+        Pila<Lanzamiento> pilaNumRepetido = new Pila<>();
+        Pila<Lanzamiento> pilaDuplicada = OperacionesPila.duplicar(pilaOriginal);
+        int repetidoDado1 = 0;
+        int repetidoDado2 = 0;
+        while (!pilaDuplicada.estaVacia()) {
+            Lanzamiento elemento1 = pilaDuplicada.desapilar();
+            Lanzamiento elemento = elemento1;
+            int valor1 = elemento.getNumDado1();
+            int valor2 = elemento.getNumDado2();
+            if (valor1 == valorRepetido) {
+                repetidoDado1++;
+            }
+            if (valor2 == valorRepetido) {
+                repetidoDado2++;
+            }
+        }
+        Lanzamiento elemento3 = new Lanzamiento(repetidoDado1, repetidoDado2);
+        pilaNumRepetido.apilar(elemento3);
+        return pilaNumRepetido;
+    }
     
 }
